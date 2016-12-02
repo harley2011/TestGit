a2b9c61c8ec24b956436778f3c8eb374b077bdf6
harley2011 <qepqwthnn35@qq.com>
2016-12-02 13:56:53 +0800

    test update

--- src/com/wll/test/TestHelloWord.java
+++ src/com/wll/test/TestHelloWord.java
@@ -4,6 +4,9 @@ public class TestHelloWord {
 
 	public static void main(String[] args) {
 		System.out.println("this is my first git project!");
+		for (String string : args) {
+			System.out.println(string);
+		}
 	}
 
 }
